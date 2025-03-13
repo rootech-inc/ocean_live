75abc605364b17320d753f2fab1ca2fb0e4beb27
Uyin Solomon <34033122+rootleet@users.noreply.github.com>
2025-03-13 15:57:07 +0000

    ralk

--- ssml/helper.py
+++ ssml/helper.py
@@ -26,14 +26,14 @@ def returns(contractor_id):
 
 def material_differences(contractor_id):
         contractor = Contractor.objects.get(id=contractor_id)
-        barcodes = IssueTransaction.objects.filter(issue__contractor=contractor).values_list('barcode', flat=True).distinct()
+        barcodes = IssueTransaction.objects.filter(issue__contractor=contractor,issue__issue_type='ISS').values_list('barcode', flat=True).distinct()
         total_value = 0
 
         transactions = []
         for barcode in barcodes:
             material = InventoryMaterial.objects.get(barcode=barcode)
             this_obj = material.obj()
-            this_obj['issued'] = IssueTransaction.objects.filter(material=material,issue__contractor=contractor).aggregate(total_qty=Sum('total_qty'))['total_qty'] or 0
+            this_obj['issued'] = IssueTransaction.objects.filter(material=material,issue__contractor=contractor,issue__issue_type='ISS').aggregate(total_qty=Sum('total_qty'))['total_qty'] or 0
             this_obj['consumed'] = MaterialOrderItem.objects.filter(material=material,service_order__contractor=contractor,material_type='is').aggregate(total_qty=Sum('quantity'))['total_qty'] or 0
             this_obj['balance'] = this_obj['issued'] - this_obj['consumed']
             negative_balance = this_obj['balance'] * -1
