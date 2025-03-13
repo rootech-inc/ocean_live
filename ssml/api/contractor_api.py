42b4a309e9556f1a210c72f5b7d673b9cb48e6e3
Uyin Solomon <34033122+rootleet@users.noreply.github.com>
2025-03-13 09:43:45 +0000

    ralk

--- ssml/api/contractor_api.py
+++ ssml/api/contractor_api.py
@@ -70,25 +70,31 @@ def contractor_api(request):
 
 
             elif module == 'returns':
-                contractor_id = data.get('contractor_id')
-                contractor = Contractor.objects.get(id=contractor_id)
-                returns = ServiceOrderReturns.objects.filter(service_order__contractor=contractor)
-                total_returns = 0
-                transactions = []
-                for rt in returns:
-                    this_obj = rt.material.obj()
-                    this_obj['expected'] = rt.quantity
-                    this_obj['returned'] = IssueTransaction.objects.filter(material=rt.material,issue__contractor=contractor,issue__issue_type='RET').aggregate(total_qty=Sum('total_qty'))['total_qty'] or 0
-                    this_obj['balance'] = this_obj['returned'] - this_obj['expected']
-                    this_obj['total_value'] = this_obj['balance'] * rt.material.value
-                    this_obj['rate'] = rt.material.value
-                    total_returns += this_obj['total_value']
-                    transactions.append(this_obj)
+                try:
+                    contractor_id = data.get('contractor_id')
+                    contractor = Contractor.objects.get(id=contractor_id)
+                    returns = ServiceOrderReturns.objects.filter(service_order__contractor=contractor).values_list('material_id',flat=True).distinct()
+                    total_returns = 0
+                    transactions = []
+                    for rt in returns:
+                        material = InventoryMaterial.objects.get(id=rt)
+                        this_obj = material.obj()
+                        this_obj['expected'] = ServiceOrderReturns.objects.filter(service_order__contractor=contractor,material=material).aggregate(tot_qty=Sum('quantity'))['tot_qty'] or 0
+                        this_obj['returned'] = IssueTransaction.objects.filter(material=material,issue__contractor=contractor,issue__issue_type='RET').aggregate(total_qty=Sum('total_qty'))['total_qty'] or 0
+                        this_obj['balance'] = this_obj['returned'] - this_obj['expected']
+                        this_obj['total_value'] = this_obj['balance'] * material.value
+                        this_obj['rate'] = material.value
+                        total_returns += this_obj['total_value']
+                        transactions.append(this_obj)
 
-                arr = {
-                    'transactions':transactions,
-                    'total':total_returns
-                }
+                    arr = {
+                        'transactions':transactions,
+                        'total':total_returns
+                    }
+                
+                except Exception as e:
+                    success_response['status_code'] = 505
+                    arr = str(e)
 
             elif module == 'credit_ledger_summary':
                 contractor_id = data.get('contractor_id')
